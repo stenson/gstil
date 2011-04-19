cffa121d484face2e2f9f18a2064e9d39eeddc04
stenson <rob.stenson@gmail.com>
2011-04-18 23:01:55 -0700

    no type checking is better than half-baked useless typechecking

--- g-stil.coffee
+++ g-stil.coffee
@@ -1,142 +1,47 @@
 # public namespace, the only thing you see
-
 window.GStil =
   makeStyle: (title) -> new Stil title
 
-# rule declarations (does type checking and serves as documentation)
-rules = (->
-  _rules =
-    featureTypes:
-      
-      administrative: [ "country", "province", "locality", "neighborhood", "land_parcel" ]
-      
-      landscape: [ "man_made", "natural" ]
-      
-      poi:
-        [ "attraction", "business", "government", "medical",
-          "park", "place_of_worship", "school", "sports_complex",
-          "road", "highway", "arterial", "local" ]
-      
-      road: [ "arterial", "highway", "local" ]
-      transit: [ "line" ]
-      "transit.station": [ "airport", "bus", "rail" ]
-      
-      water: true
-    
-    elementTypes: [ "all", "geometry", "labels" ]
-    
-    stylers:    
-      # any type of color, or just one kind?
-      hue: /#[abcdefABCDEF0123456789]{6}/
-      # quick invert, easy boolean
-      invert_lightness: [ true, false ]
-      # all the same in terms of range from -100 to 100
-      saturation: "-100..100"
-      lightness: "-100..100"
-      gamma: "-100..100"
-      visibility: [ "on", "simplified", "off" ]
-
-  # function for avoiding redundancy in spec above
-  features = _rules.featureTypes
-  for supr,subs of features
-    features["#{supr}.#{sub}"] = true for sub in subs
-    features[supr] = true
-    
-  # return the rules we've modified
-  return _rules;
-)()
-
 # the stil object (whose methods you use, but don't really see)
-
 class Stil
-  
   # title is required by google
   constructor: (name) ->
     @name = name or Math.random().toString()
     @rules = []
-    @last = null
-    return @map
-    
   # add a rule to the rule set
   pushRule: (rule) ->
     @rules.push rule
     @last = rule
-  
-  # private method for building the styled map type
-  _buildStyledMapType: (map) ->
-    @map = map # cache this reference
-    new google.maps.StyledMapType @rules,
-      map: map,
-      name: @name
-    
   # add the style to this map when the rules are done
   addTo: (map) ->
-    @map = map
-    # google maps code on setting
-    setTimeout (=>
-      style = @._buildStyledMapType map
+    @map ?= map
+    setTimeout (=> # google maps code for setting
+      @style = new google.maps.StyledMapType @rules,
+        map: @map,
+        name: @name
       # add to the map registry
-      map.mapTypes.set @name, style
-      map.setMapTypeId @name
-      @style = style
+      @map.mapTypes.set @name, @style
+      @map.setMapTypeId @name
     ),0
     return @ # chain
-  
-  # create the style without explicitly adding it to the map
-  # should be called after your rules are set up
-  registerWith: (map) ->
-    @.style = @._buildStyledMapType map
-    return @
-  
-  update: ->
-    @addTo @map # re-trigger the bind
-    
   # the initial selection from the main map
   rule: (featureType, stylers) ->
-    _features = rules.featureTypes
-    rule = null
-    # test to see if the rule specified is a real rule
-    if _features.hasOwnProperty featureType
-      rule = makeRule featureType
-    else # maybe, if we consider the context, this is just a suffix
-      prefix = @last.featureType.split(".")[0]
-      suffix = featureType
-      compoundType = "#{prefix}.#{suffix}"
-      if _features.hasOwnProperty compoundType
-        rule = makeRule compoundType
-    # if there's a rule, push it
-    if rule isnt null
-      @pushRule rule
-      if stylers
-        @[key](value) for key,value of stylers # set with native method
-    else
-      throw "That's not a real feature type."
+    @pushRule
+      featureType: featureType
+      elementType: "all",
+      stylers: []
+    @[key](value) for key,value of stylers if stylers
     return @ # chain on
-    
-  # refine the last featureType by elementType
-  just: (elementType) ->
-    @last.elementType = elementType
-    return @ # don't break the chain
-  
   # turn off a bunch of stuff (common task)
   turnOff: (_features...) ->
-    @rule(feature).visibility("off") for feature in _features
+    @rule(f).visibility("off") for f in _features
     return @ # chain
 
-# now dynamically add methods for each styler
-addStyle = (name,type) ->
-  Stil::[name] = (style) ->
-    obj = {}
-    obj[name] = style
-    @last.stylers.push obj
-    return @
-
-addStyle(name,type) for name,type of rules.stylers
-
-
-# the rule object (which you don't see)
-
-makeRule = (featureType) ->
-  featureType: featureType,
-  elementType: "all", # default
-  stylers: []
+# dynamically add methods for each styler
+for style in ["hue","invert_lightness","saturation","lightness","gamma","visibility"]
+  (->
+    Stil::[style] = (_style) ->
+      obj = {}; obj[style] = _style
+      @last.stylers.push obj
+      return @
+  )()
--- g-stil.js
+++ g-stil.js
@@ -1,141 +1,74 @@
 (function() {
-  var Stil, addStyle, makeRule, name, rules, type, _ref;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __slice = Array.prototype.slice;
+  var Stil, _i, _len, _ref;
+  var __bind = function(func, context) {
+    return function(){ return func.apply(context, arguments); };
+  }, __hasProp = Object.prototype.hasOwnProperty, __slice = Array.prototype.slice;
   window.GStil = {
     makeStyle: function(title) {
       return new Stil(title);
     }
   };
-  rules = (function() {
-    var features, sub, subs, supr, _i, _len, _rules;
-    _rules = {
-      featureTypes: {
-        administrative: ["country", "province", "locality", "neighborhood", "land_parcel"],
-        landscape: ["man_made", "natural"],
-        poi: ["attraction", "business", "government", "medical", "park", "place_of_worship", "school", "sports_complex", "road", "highway", "arterial", "local"],
-        road: ["arterial", "highway", "local"],
-        transit: ["line"],
-        "transit.station": ["airport", "bus", "rail"],
-        water: true
-      },
-      elementTypes: ["all", "geometry", "labels"],
-      stylers: {
-        hue: /#[abcdefABCDEF0123456789]{6}/,
-        invert_lightness: [true, false],
-        saturation: "-100..100",
-        lightness: "-100..100",
-        gamma: "-100..100",
-        visibility: ["on", "simplified", "off"]
-      }
-    };
-    features = _rules.featureTypes;
-    for (supr in features) {
-      subs = features[supr];
-      for (_i = 0, _len = subs.length; _i < _len; _i++) {
-        sub = subs[_i];
-        features["" + supr + "." + sub] = true;
-      }
-      features[supr] = true;
-    }
-    return _rules;
-  })();
-  Stil = (function() {
-    function Stil(name) {
-      this.name = name || Math.random().toString();
-      this.rules = [];
-      this.last = null;
-      return this.map;
-    }
-    Stil.prototype.pushRule = function(rule) {
-      this.rules.push(rule);
-      return this.last = rule;
-    };
-    Stil.prototype._buildStyledMapType = function(map) {
-      this.map = map;
-      return new google.maps.StyledMapType(this.rules, {
-        map: map,
+  Stil = function(name) {
+    this.name = name || Math.random().toString();
+    this.rules = [];
+    return this;
+  };
+  Stil.prototype.pushRule = function(rule) {
+    this.rules.push(rule);
+    return (this.last = rule);
+  };
+  Stil.prototype.addTo = function(map) {
+    this.map = (typeof this.map !== "undefined" && this.map !== null) ? this.map : map;
+    setTimeout(__bind(function() {
+      this.style = new google.maps.StyledMapType(this.rules, {
+        map: this.map,
         name: this.name
       });
-    };
-    Stil.prototype.addTo = function(map) {
-      this.map = map;
-      setTimeout((__bind(function() {
-        var style;
-        style = this._buildStyledMapType(map);
-        map.mapTypes.set(this.name, style);
-        map.setMapTypeId(this.name);
-        return this.style = style;
-      }, this)), 0);
-      return this;
-    };
-    Stil.prototype.registerWith = function(map) {
-      this.style = this._buildStyledMapType(map);
-      return this;
-    };
-    Stil.prototype.update = function() {
-      return this.addTo(this.map);
-    };
-    Stil.prototype.rule = function(featureType, stylers) {
-      var compoundType, key, prefix, rule, suffix, value, _features;
-      _features = rules.featureTypes;
-      rule = null;
-      if (_features.hasOwnProperty(featureType)) {
-        rule = makeRule(featureType);
-      } else {
-        prefix = this.last.featureType.split(".")[0];
-        suffix = featureType;
-        compoundType = "" + prefix + "." + suffix;
-        if (_features.hasOwnProperty(compoundType)) {
-          rule = makeRule(compoundType);
-        }
-      }
-      if (rule !== null) {
-        this.pushRule(rule);
-        if (stylers) {
-          for (key in stylers) {
-            value = stylers[key];
-            this[key](value);
-          }
-        }
-      } else {
-        throw "That's not a real feature type.";
-      }
-      return this;
-    };
-    Stil.prototype.just = function(elementType) {
-      this.last.elementType = elementType;
-      return this;
-    };
-    Stil.prototype.turnOff = function() {
-      var feature, _features, _i, _len;
-      _features = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      for (_i = 0, _len = _features.length; _i < _len; _i++) {
-        feature = _features[_i];
-        this.rule(feature).visibility("off");
-      }
-      return this;
-    };
-    return Stil;
-  })();
-  addStyle = function(name, type) {
-    return Stil.prototype[name] = function(style) {
-      var obj;
-      obj = {};
-      obj[name] = style;
-      this.last.stylers.push(obj);
-      return this;
-    };
+      this.map.mapTypes.set(this.name, this.style);
+      return this.map.setMapTypeId(this.name);
+    }, this), 0);
+    return this;
   };
-  _ref = rules.stylers;
-  for (name in _ref) {
-    type = _ref[name];
-    addStyle(name, type);
-  }
-  makeRule = function(featureType) {
-    return {
+  Stil.prototype.rule = function(featureType, stylers) {
+    var _ref, key, value;
+    this.pushRule({
       featureType: featureType,
       elementType: "all",
       stylers: []
-    };
+    });
+    if (stylers) {
+      _ref = stylers;
+      for (key in _ref) {
+        if (!__hasProp.call(_ref, key)) continue;
+        value = _ref[key];
+        this[key](value);
+      }
+    }
+    return this;
   };
+  Stil.prototype.turnOff = function() {
+    var _features, _i, _len, _ref, f;
+    _features = __slice.call(arguments, 0);
+    _ref = _features;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      f = _ref[_i];
+      this.rule(f).visibility("off");
+    }
+    return this;
+  };
+  _ref = ["hue", "invert_lightness", "saturation", "lightness", "gamma", "visibility"];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    (function() {
+      var style = _ref[_i];
+      return (function() {
+        return (Stil.prototype[style] = function(_style) {
+          var obj;
+          obj = {};
+          obj[style] = _style;
+          this.last.stylers.push(obj);
+          return this;
+        });
+      })();
+    })();
+  }
 }).call(this);
